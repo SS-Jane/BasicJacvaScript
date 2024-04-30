564cea92a3e7e4a23f721d177b2edef13fff95a8
SS-Jane <superjane.prodev@gmail.com>
2024-04-30 14:41:05 +0700

    add javascript variable

--- app.js
+++ app.js
@@ -20,3 +20,35 @@ console.log(name);
 document.write(name);
 console.log("Age :",age);
 
+let money = 500;
+let vat = 0.07;
+
+console.log(money);
+console.log(vat);
+console.log(money*vat);
+
+
+console.log(typeof(money*vat))
+
+console.log(money>vat)
+console.log(typeof(money>vat))
+
+
+// type convert string --> number
+
+let age2 = "30";
+console.log(age2,",",typeof(age2))
+
+let age3 = +"30";
+console.log(age3,",",typeof(age3))
+
+// type convert number --> string
+let price1 = 2057.25;
+console.log(price1,",",typeof(price1))
+
+let price2 = 2057.25.toString();
+console.log(price2,",",typeof(price2))
+
+
+
+
