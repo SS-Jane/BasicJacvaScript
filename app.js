34b519e29ce7464417b184a604d2d8b73b8816aa
SS-Jane <superjane.prodev@gmail.com>
2024-04-30 13:19:14 +0700

    add javascript syntex comment naming_format define_variable

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-document.write("<h1>JavaScript By Superjane</h1>")
-document.write("<h1>This write by JavaScript</h1>")
+document.write("<h1>JavaScript By Superjane</h1>");
+document.write("<h1>This write by JavaScript</h1>");
+
+// alert("Hi! Hi!");
+// white color text
+console.log("Hello Javascript");
+// red color text
+console.error("Found malware!!");
+// yellow color text
+console.warn("Found Mistake informatiob");
+
+// variable
+
+let name;
+name = "SuperJane";
+let age;
+age = 30;
+
+console.log(name);
+document.write(name);
+console.log("Age :",age);
+
