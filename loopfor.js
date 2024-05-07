1b9466039fdcb638fbe212b712235dd75acde453
SS-Jane <superjane.prodev@gmail.com>
2024-05-08 06:33:30 +0700

    add loop

---
+++ loopfor.js
@@ -0,0 +1,6 @@
+for (let count = 0 ; count <= 10 ; count++) {
+    console.log(count,"Hello work!!");
+    if (count == 5) {
+        break;
+    }
+}
---
+++ loopwhile.js
@@ -0,0 +1,9 @@
+let count = 1;
+
+while (count <= 10){
+    console.log("Hi Hi : ",count);
+    if (count == 4){
+        break;
+    }
+    count++;
+}
